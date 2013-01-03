0f822eb10c0e2ed63e139ee0b6e5a2e35e99a2c0
Ionica Bizau <bizauionica@yahoo.com>
2013-01-03 15:30:03 +0200

    Added an allert if error exists.

--- main.js
+++ main.js
@@ -5,22 +5,29 @@ define([
 
     function init(config) {
         self = this;
-        debugger;
-        self.link("applications", function(data) {
+        self.link("applications", function(err, data) {
+            if(err) return showError(err);
             buildTable(data);
         });
     }
+    
+    function buildTable(apps) {   
+        var template = $(".template");
+        var tbody = $("#appsTable").find("tbody");
+        for(var i in apps) {
+            var app = apps[i];
+            var tr = template.clone();
+            tr.attr = app.id;
+            tr.find('.name').find("a").html(app.name);
+            tbody.append(tr);
+        }
+    }
+    
+    function showError(err) {
+        $("#errorAlert").text(err).show();
+    }
+    
     return init;
 });
 
-function buildTable(apps) {   
-    var template = $(".template");
-    var tbody = $("#appsTable").find("tbody");
-    for(var i in apps) {
-        var app = apps[i];
-        var tr = template.clone();
-        tr.attr = app.id;
-        tr.find('.name').find("a").html(app.name);
-        tbody.append(tr);
-    }
-}
+
