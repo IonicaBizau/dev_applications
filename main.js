c97ce59dbcad1ea1cfe00b39d2c9bd474cc5255b
Ionica Bizau <bizauionica@yahoo.com>
2013-01-03 14:27:25 +0200

    Optimized the buildTable function.

--- main.js
+++ main.js
@@ -12,69 +12,14 @@ define([
     return init;
 });
 
-function buildTable(apps) {
-    var appsTable = $("#appsTable");
-    
-    // Redeploy button
-    var redeployButton = $("<button class='btn btn-primary'>");
-    var redeployIcon = $("<i class='icon-upload icon-white'></i>");
-    redeployButton.append(redeployIcon);
-    redeployButton.append(" Redeploy"); 
-       
-    // Start button
-    var startButton = $("<button class='btn btn-success'>");
-    var startIcon = $("<i class='icon-play icon-white'></i>");
-    startButton.append(startIcon);
-    startButton.append(" Start"); 
-    
-    // Edit button
-    var editButton = $("<button class='btn btn-warning'>");
-    var editIcon = $("<i class='icon-edit icon-white'></i>");
-    editButton.append(editIcon);
-    editButton.append(" Edit"); 
-    
-    // Delete button
-    var deleteButton = $("<button class='btn btn-danger'>");
-    var deleteIcon = $("<i class='icon-trash icon-white'></i>");
-    deleteButton.append(deleteIcon);
-    deleteButton.append(" Delete"); 
-    
-    // HTML Table
-    var htmlTable = $("<table class='table table-bordered'>");
-    var thead = $("<thead>");
-    var tr = $("<tr>");
-
-    var th1 = $("<th style='width:60%'>");
-    th1.html("Application Name");
-
-    var th2 = $("<th>");
-    th2.html("Operations");
-    
-    tr.append(th1);
-    tr.append(th2);
-    
-    thead.append(tr);
-
-    var tbody = $("<tbody>");
-    
+function buildTable(apps) {   
+    var template = $(".template");
+    var tbody = $("#appsTable").find("tbody");
     for(var i in apps) {
-        var row = $("<tr>");
-
-        var col1 = $("<td>");
-        col1.html(i + ". " + apps[i].name);
-        
-        var col2 = $("<td>");
-        col2.append(redeployButton);
-        col2.append(startButton);
-        col2.append(editButton);
-        col2.append(deleteButton);
-        
-        row.append(col1);
-        row.append(col2);
-        
-        tbody.append(row);
+        var app = apps[i];
+        var tr = template.clone();
+        tr.attr = app.id;
+        tr.find('.name').find("a").html(app.name);
+        tbody.append(tr);
     }
-    
-    htmlTable.append(tbody);
-    appsTable.append(htmlTable);
 }
