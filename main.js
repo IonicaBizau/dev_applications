e376457590eed92cbdad9b53f7b97489bce6e15a
Ionica Bizau <bizauionica@yahoo.com>
2013-03-07 19:54:31 +0200

    Fixed Update operation. Added info alerts.

--- main.js
+++ main.js
@@ -5,41 +5,34 @@ define([
     var self;
     
     var FADE_TIME = 500;
+    var appsArray = [];
     
     function init(config) {
         self = this;
-        
-        // Click on redeployMonoDev button
-        $("#redeployMonoDev").on("click", function() {
-            var redeployButton = $(this);
-            redeployButton.button("loading");
-            self.link("redeployMonoDev", function(err) {
-                if (err) {
-                    showError("Failed to redeploy MonoDev.");
-                    return;
-                }
 
-                redeployButton.button("reset");
-                console.log("Successfully repdeployed.");
-            });
-        });
-        
+        var redeployButton = $("#redeployMonoDev");
+                
         // Get the applications data
         self.link("applications", function(err, data) {
-            if(err) return showError(err);
+            if (err) return showError(err);
+            appsArray = data;
+            
             buildTable(data);
         });
         
         var appId;
         
         // Click on an operation button
-        $('#appsTable').on("click", ".btn", function() {
+        $("#redeployMonoDev").on("click", function() {
+            confirmAction("redeployMonoDev");
+        });
 
+        $("#appsTable").on("click", ".btn", function() {
             var operation = $(this).attr("data-operation");
             
             appId = $(this).closest("[id]").attr("id");
 
-            if(operation) {
+            if (operation) {
                 confirmAction(operation);
             }
         });
@@ -51,26 +44,53 @@ define([
             $("#"+appId).find(".spinner").show();
             $("#"+appId).find(".operations").hide();
             $("#modal").modal("hide");
-                                        
+
+            var successLabel = " <span class='label label-success'>Success</span>";
+            var infoLabel = " <span class='label label-info'>Info</span>";
+            var errorLabel = " <span class='label label-important'>Error</span>";
+            
+            var icon = "<i class='icon-info-sign'></i> ";
+            
+            showStartOperationMessage(icon + "Started <strong>" + $("#operationName").text() + "</strong> operation for <strong>" + getAppNameById(appId) + "</strong>" + infoLabel);
+
             switch(operationName){
                 case "delete":
                     self.link("delete", { data : appId }, function(err, data) {
-                        if(err) return showError(err);
+                        var icon = "<i class='icon-trash'></i> ";
+                        if (err) return showError(icon + err + errorLabel);
+                        var message = icon + "<strong>" + getAppNameById(appId) + "</strong> successfully <strong>deleted</strong>." + successLabel;
+                        showSuccessMessage(message);
                         $("#" + appId).fadeOut(FADE_TIME);
                     });
                 break;
                 case "redeploy":
                     self.link("redeploy", { data: appId }, function(err, data) {
+                        var icon = "<i class='icon-refresh'></i> ";
                         $("#"+appId).find(".operations").show();
                         $("#"+appId).find(".spinner").hide();
-                        if(err) return showError(err);
+                        if (err) return showError(icon + err + errorLabel);
+                        var message = icon + "<strong>" + getAppNameById(appId) + "</strong> successfully <strong>redeployed</strong>." + successLabel;
+                        showSuccessMessage(message);
+                    });
+                break;
+                case "redeploy monodev":
+                    redeployButton.button("loading");
+                    self.link("redeployMonoDev", { data: appId }, function(err, data) {
+                        redeployButton.button("reset");
+                        var icon = "<i class='icon-refresh'></i> ";
+                        if (err) return showError(icon + err + errorLabel);
+                        var message = icon + "<strong>MonoDev</strong> successfully<strong> redeployed. Refresh the page.</strong>." + successLabel;
+                        showSuccessMessage(message);
                     });
                 break;
                 case "update":
                     self.link("update", { data: appId }, function(err, data) {
+                        var icon = "<i class='icon-download'></i> ";
                         $("#"+appId).find(".operations").show();
                         $("#"+appId).find(".spinner").hide();
-                        if(err) return showError(err);
+                        if (err) return showError(icon + err + errorLabel);
+                        var message = icon + "<strong>" + getAppNameById(appId) + "</strong> successfully <strong>updated</strong>." + successLabel;
+                        showSuccessMessage(message);
                     });
                 break;
 
@@ -85,7 +105,7 @@ define([
         for(var i in apps) {
             var app = apps[i];
             
-            if(app.name !== "MonoDev") {
+            if (app.name !== "MonoDev") {
                 var tr = template.clone().removeClass("template").show();
                 tr.attr("id", app.id);
                 tr.find('.name').find("a").html(app.name);
@@ -96,7 +116,30 @@ define([
     
     // Show error
     function showError(err) {
-        $("#errorAlert").html("Error: " + err).fadeIn();
+        var template = $("#errorAlert").clone().attr("id", "");
+        template.find(".message").html(err);
+
+        template.fadeIn();
+        
+        $("#alerts").append(template);
+    }
+    
+    function showStartOperationMessage(message) {
+        var template = $("#warningAlert").clone().attr("id", "");
+
+        template.find(".message").html(message);
+        template.fadeIn();
+        
+        $("#alerts").append(template);
+    }
+    
+    function showSuccessMessage(message) {
+        var template = $("#successAlert").clone().attr("id", "");
+
+        template.find(".message").html(message);
+        template.fadeIn();
+        
+        $("#alerts").append(template);
     }
     
     // Confirm Modal
@@ -112,6 +155,11 @@ define([
                 $("#question").html("Are you sure that you want to redeploy this application?");
                 $("#modal").modal('show');
             break;
+            case "redeployMonoDev":
+                $("#operationName").html("Redeploy MonoDev");
+                $("#question").html("Are you sure that you want to redeploy <strong>MonoDev</strong>?");
+                $("#modal").modal('show');
+            break;
             case "update":
                 $("#operationName").html("Update");
                 $("#question").html("Are you sure that you want to update this application?");
@@ -120,7 +168,15 @@ define([
         }
     }
     
+    // Get application name by id
+    function getAppNameById(appId) {
+        for (var app in appsArray) {
+            if (appsArray[app].id === appId) {
+                return appsArray[app].name;
+            }
+        }
+        return "";
+    }
+    
     return init;
 });
-
-
--- operations.js
+++ operations.js
@@ -3,7 +3,6 @@ var spawn = require("child_process").spawn;
 var send  = require(CONFIG.root + "/core/send.js").send;
 var apps = require(CONFIG.root + "/api/apps");
 
-
 // Redeploy app function
 exports.redeploy = function(link) {
 
@@ -39,27 +38,43 @@ exports.edit = function(link) {
 
 // Update app function
 exports.update = function(link) {
+    // Application ID
     var appId = link.data;
 
+    // Start the Update operation
     console.log("------------");
     console.log("Starting UPDATE operation...");
-            
-    var deployer = spawn("node", [CONFIG.root + "/admin/scripts/installation/update_app.js", CONFIG.APPLICATION_ROOT + "/" + appId]);
 
-    deployer.stderr.pipe(process.stderr);
-    deployer.stdout.pipe(process.stdout);
+    var updater = spawn("node", [CONFIG.root + "/admin/scripts/installation/update_app.js", CONFIG.APPLICATION_ROOT + "/" + appId]);
 
-    deployer.on('exit', function(code) {
+    updater.stderr.pipe(process.stderr);
+    updater.stdout.pipe(process.stdout);
+
+    // Finished the operation
+    updater.on('exit', function(code) {
         if (code) {
-            send.internalservererror(link, "Update failed for application: " + appId);
+            console.log(">>>>> Code: " + code);
+            
+            var errors = [
+                "",
+                "Please provide an application id as argument.",
+                "This application doesn't contain the repository field.",
+                "This application doesn't contain the repository.url field.",
+                "Failed to make temp directory.",
+                "Download of zip failed.",
+                "The application was not found in the databse. Application deployment failed somehow. :(",
+                "Unknown reason. Maybe if you check @/core/getLog you will see the error.",
+                "Failed to connect to OrientDB",
+                "The ID doesn't exist in database. Update failed."
+            ];
+            
+            send.internalservererror(link, "Update failed for application: " + appId + ". Error: " + errors[code] + " Error code: " + code);
         } else {
             console.log("Application " + appId + " successfully updated.");
             console.log("------------");
             send.ok(link.res, "Application " + appId + " successfully updated.");
         }
     });
-
-//    send.ok(link.res, "OK");
 }
 
 // Delete app function
@@ -80,6 +95,7 @@ exports.delete = function(link) {
 // Get apps names function
 exports.applications = function(link) {
     apps.getApplications(function(err, appsObjects){
+        console.log(appsObjects);
 
         if (err) {
             return send.internalservererror(link, err);
