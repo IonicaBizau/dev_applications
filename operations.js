1f0e25ce7fe2f2c06e27ad611e6c4ec73d6aa288
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-04-10 19:04:34 +0200

    Added update app functionality

--- operations.js
+++ operations.js
@@ -4,6 +4,7 @@ var spawn = require("child_process").spawn;
 exports.redeploy = function(link) {
 
     var appId = link.data;
+
     M.app.redeploy(appId, function(err) {
 
         if (err) {
@@ -32,41 +33,16 @@ exports.edit = function(link) {
 
 // Update app function
 exports.update = function(link) {
-    // Application ID
+
     var appId = link.data;
 
-    // Start the Update operation
-    console.log("------------");
-    console.log("Starting UPDATE operation...");
+    M.app.update(appId, function(err) {
 
-    var updater = spawn("node", [M.config.root + "/admin/scripts/installation/update_app.js", M.config.APPLICATION_ROOT + "/" + appId]);
-
-    updater.stderr.pipe(process.stderr);
-    updater.stdout.pipe(process.stdout);
-
-    // Finished the operation
-    updater.on('exit', function(code) {
-        if (code) {
-            
-            var errors = [
-                "",
-                "Please provide an application id as argument.",
-                "This application doesn't contain the repository field.",
-                "This application doesn't contain the repository.url field.",
-                "Failed to make temp directory.",
-                "Download of zip failed.",
-                "The application was not found in the databse. Application deployment failed somehow. :(",
-                "Unknown reason. Maybe if you check @/core/getLog you will see the error.",
-                "Failed to connect to OrientDB",
-                "The ID doesn't exist in database. Update failed."
-            ];
-            
-            link.send(500, "Update failed for application: " + appId + ". Error: " + errors[code] + " Error code: " + code);
-        } else {
-            console.log("Application " + appId + " successfully updated.");
-            console.log("------------");
-            link.send(200, "Application " + appId + " successfully updated.");
+        if (err) {
+            return link.send(500, err.message);
         }
+
+        link.send(200)
     });
 }
 
@@ -83,10 +59,11 @@ exports.delete = function(link) {
 
         link.send(200)
     });
-}
+};
 
 // Get apps names function
 exports.applications = function(link) {
+
     M.app.getApplications(function(err, appsObjects){
 
         if (err) {
@@ -97,7 +74,7 @@ exports.applications = function(link) {
 
         link.send(200, appsObjects);
     });
-}
+};
 
 // Redeploy MonoDev
 exports.redeployMonoDev = function(link) {
@@ -118,7 +95,7 @@ exports.redeployMonoDev = function(link) {
         }
         console.log("-- MonoDev Redeployment Ended --");
     });
-}
+};
 
 // Sort alphabetically
 function sortAppsArray(array) {
